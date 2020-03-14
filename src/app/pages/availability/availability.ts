2385aa4c146faa36e30ba6bccfd6563ee40c76bd
Mardi Higgerson <mardi@helka.com.au>
2020-03-14 20:12:54 +1100

    added UI Componants for availability

--- src/app/pages/availability/availability.ts
+++ src/app/pages/availability/availability.ts
@@ -20,85 +20,22 @@ import {
   styleUrls: ['./availability.scss'],
 })
 export class AvailabilityPage implements OnInit {
-  constructor(public modalCtrl: ModalController) { }
-
+  
   ngOnInit() {
+
   }
-
-  dateRange: { from: '2019-05-02'; to: '2019-05-07'; };
-  type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
-  optionsRange: CalendarComponentOptions = {
-    pickMode: 'range'
-  };
-
+  dateMulti: string[];
+  type: 'string';
+  
   optionsMulti: CalendarComponentOptions = {
     pickMode: 'multi'
   };
 
-  async basic()
-  {
-    const options: CalendarModalOptions = {
-      title: 'BASIC',
-      color:'dark'
-    };
+  constructor() { }
 
-    const myCalendar = await this.modalCtrl.create({
-      component: CalendarModal,
-      componentProps: { options }
-      
-    });
-
-    myCalendar.present();
-
-    const event: any = await myCalendar.onDidDismiss();
-    const date: CalendarResult = event.data;
-    console.log(date);
-  
+  button(){
+    console.log(this.dateMulti);
   }
-
-  async multiple()
-  {
-    const options = {
-      pickMode: 'multi',
-      title: 'MULTI',
-    };
-  
-    const myCalendar = await this.modalCtrl.create({
-      component: CalendarModal,
-      componentProps: { options },
-      cssClass:'mbg'
-    });
-  
-    myCalendar.present();
-  
-    const event: any = await myCalendar.onDidDismiss();
-    const date: CalendarResult = event.data;
-    console.log(date);
-  }
-
-  async range()
-  {
-    const options: CalendarModalOptions = {
-      pickMode: 'range',
-      title: 'RANGE',
-      color:'danger'
-    };
-  
-    const myCalendar = await this.modalCtrl.create({
-      component: CalendarModal,
-      componentProps: { options }
-    });
-  
-    myCalendar.present();
-  
-    const event: any = await myCalendar.onDidDismiss();
-    const date = event.data;
-    const from: CalendarResult = date.from;
-    const to: CalendarResult = date.to;
-  
-    console.log(date, from, to);
-  }
-
 }
   
 //   // Gets a reference to the list element
