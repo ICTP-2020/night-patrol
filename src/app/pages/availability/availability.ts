25bdacb2380ddd2b1e9b94608bf9c152d0d0c071
Mardi Higgerson <mardi@helka.com.au>
2020-03-18 12:48:56 +1100

    add demo data to availability

--- src/app/pages/availability/availability.ts
+++ src/app/pages/availability/availability.ts
@@ -8,7 +8,7 @@ import {
   styleUrls: ['./availability.scss'],
 })
 export class AvailabilityPage implements OnInit {
-  dateMulti: string[];
+  dateMulti: string[] = ["2020-03-03T13:00:00.000Z","2020-03-18T13:00:00.000Z","2020-03-23T13:00:00.000Z","2020-03-25T13:00:00.000Z"]
   type: 'string';
   optionsMulti: CalendarComponentOptions = {
     pickMode: 'multi'
